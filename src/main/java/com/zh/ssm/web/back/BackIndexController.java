1be00921ddd86dcc1254fab9b0b266c6c792ea5c
DELL <444296812@qq.com>
2016-11-23 12:22:43 +0800

    丽江古城环境，删除了多余的代码类。

--- src/main/java/com/zh/ssm/web/back/BackIndexController.java
+++ src/main/java/com/zh/ssm/web/back/BackIndexController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/back")
 public class BackIndexController {
     @Autowired
-    private UserService userService;
+
 
     @RequestMapping("/main")
     public String get1(){
